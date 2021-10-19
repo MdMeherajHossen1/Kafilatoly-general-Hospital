0ab26c63f971211d74e833fce8d3f85ddc9b6c29
MeherajMuhammad <joycomputers36@gmail.com>
2021-10-19 19:36:09 +0300

    done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Banner.css'
 const Banner = () => {
     return (
-        <div className="banner-bg flex justify-center align-center sm:mt-32">
+        <div className="banner-bg flex justify-center align-center">
             <div className="md:w-3/4 sm:px-2 h-50 p-2  rounded text-white mt-4 ">
                 <h3 > Welcome to <span className="text-yellow-400 border-b-2 border-red-600">KAFILATOLY GENERAL HOSPITAL</span></h3>
                 <h4 className="lg:text-6xl sm:text-3xl">Improving the lives of our patients and their Famillies</h4>
